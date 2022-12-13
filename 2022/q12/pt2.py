a695cd3bf1191b8663cba69bce5241ecf4546cdb
Philip Montgomery <pmontgom@broadinstitute.org>
2022-12-12 21:03:56 -0500

    day 12

---
+++ 2022/q12/pt2.py
@@ -0,0 +1,75 @@
+#-*-coding:utf8;-*-
+import sys
+
+with open(sys.argv[1], "rt") as fd:
+    m = [x.strip() for x in fd.read().split("\n") if x.strip() != ""]
+    h = len(m)
+    w = len(m[0])
+    lowest = []
+    for y in range(h):
+        for x in range(w):
+            height = m[y][x]
+            if height == 'a':
+                lowest.append((x,y))
+            if height == 'S':
+                start_pos = (x,y)
+            elif height == 'E':
+                dest_pos = (x,y)
+
+def heightat(pos):
+    x,y=pos
+    h = m[y][x]
+    if h in ['S']:
+        h = 'a'
+    elif h == 'E':
+        h = 'z'
+    return ord(h)
+
+def getexits(pos):
+    exits = []
+    x,y=pos
+    height = heightat(pos)
+    for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+     nx=dx+x
+     ny=dy+y
+     if nx >= 0 and ny >= 0 and nx < w and ny < h:
+#         print(f"height={height} heightat((nx,ny))={heightat((nx,ny))}")
+         if height +1 >= heightat((nx,ny)):
+          exits.append((nx,ny))
+    return exits
+
+def bfs(dst):
+ queue = list(lowest)
+ paths={}
+ for pos in lowest:
+    paths[pos]=[]
+# breakpoint()
+ while True:
+  pos = queue[0]
+  del queue[0]
+  
+  exits = getexits(pos)
+#  print("exits", exits)
+  for exit in exits:
+   if exit in paths:
+    if len(paths[exit])>len(paths[pos])+1:
+     paths[exit] = paths[pos] +[exit]
+   else:
+    paths[exit] = paths[pos] +[exit]
+    queue.append(exit)
+  if len(queue) ==0:
+   break
+  
+ return paths
+
+paths = bfs(dest_pos)
+#from_lowest=[]
+#for pos in lowest:
+#    from_lowest.append( (len(paths[pos]), pos) )
+
+#print(sorted(from_lowest)[:10])
+
+#print([ chr(heightat(x)) for x in paths[dest_pos] ])
+#print(getexits((4,0)))
+
+print(len(paths[dest_pos]))
